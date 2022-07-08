b2c3a87ef35e20bffddc7ce0bf780d23a6bdca65
Eyüp Demir <eyupdemir@gmail.com>
2022-07-08 09:18:22 +0300

    Got real posts data from database instead of dummy data

--- components/Posts.js
+++ components/Posts.js
@@ -1,33 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { onSnapshot, collection, query, orderBy } from 'firebase/firestore';
+
 import Post from './Post';
+import { db } from '../firebase';
 
 function Posts() {
-  const posts = [
-    {
-      id: '1',
-      username: 'mithderler',
-      userImg:
-        'https://lh3.googleusercontent.com/a-/AOh14Gh3Hk1zzy8sZ7Uolrq78h3tfbo3GjG6oiNKu2lf6Q=s96-c',
-      img: 'https://images.unsplash.com/photo-1657107154933-0822c6695f6d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80',
-      caption: 'Thats a great picture',
-    },
-    {
-      id: '2',
-      username: 'mithderler',
-      userImg:
-        'https://lh3.googleusercontent.com/a-/AOh14Gh3Hk1zzy8sZ7Uolrq78h3tfbo3GjG6oiNKu2lf6Q=s96-c',
-      img: 'https://images.unsplash.com/photo-1657015232876-678d7cfeec2e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
-      caption: 'Great setup for working',
-    },
-  ];
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      query(collection(db, 'posts'), orderBy('timestamp', 'desc')),
+      (snapshot) => {
+        const fetchedPosts = snapshot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        setPosts(fetchedPosts);
+      }
+    );
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
   return (
     <div>
       {posts.map((post) => (
         <Post
           key={post.id}
           username={post.username}
-          userImg={post.userImg}
-          img={post.img}
+          userImg={post.profileImg}
+          img={post.image}
           caption={post.caption}
         />
       ))}
